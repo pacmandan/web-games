62ef3581088e5734af1b726eb8bfe933b9ae9d7b
Daniel Green <dgreen314@gmail.com>
2023-12-14 17:18:40 -0700

    Updated logger to JSON, generated Dockerfile

--- config/config.exs
+++ config/config.exs
@@ -44,6 +44,11 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :logger_json, :backend,
+  metadata: :all,
+  json_encoder: Jason,
+  formatter: LoggerJSON.Formatters.GoogleCloudLogger
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
---
+++ rel/overlays/bin/server.bat
@@ -0,0 +1,2 @@
+set PHX_SERVER=true
+call "%~dp0\web_games" start
