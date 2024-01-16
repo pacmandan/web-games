9758ccf1922f9532ef2f262dde10097d11fa3e4c
Daniel Green <dgreen314@gmail.com>
2024-01-15 22:29:52 -0700

    Added the last of the Minesweeper GameState tests and InternalComms tests.

--- test/web_games/minesweeper/game_state_test.exs
+++ test/web_games/minesweeper/game_state_test.exs
@@ -22,33 +22,7 @@ defmodule WebGames.Minesweeper.GameStateTest do
       # 4|.1X1.
       #
       # y
-      #
-      grid: %{
-        {1,1} => %Cell{value: 1},
-        {1,2} => %Cell{value: 1, has_mine?: true},
-        {1,3} => %Cell{value: 1},
-        {1,4} => %Cell{},
-
-        {2,1} => %Cell{value: 1},
-        {2,2} => %Cell{value: 1},
-        {2,3} => %Cell{value: 2},
-        {2,4} => %Cell{value: 1},
-
-        {3,1} => %Cell{},
-        {3,2} => %Cell{},
-        {3,3} => %Cell{value: 1},
-        {3,4} => %Cell{value: 1, has_mine?: true},
-
-        {4,1} => %Cell{},
-        {4,2} => %Cell{},
-        {4,3} => %Cell{value: 1},
-        {4,4} => %Cell{value: 1},
-
-        {5,1} => %Cell{},
-        {5,2} => %Cell{},
-        {5,3} => %Cell{},
-        {5,4} => %Cell{},
-      },
+      grid: mined_grid(),
       status: :init,
       notifications: [],
       player: nil,
@@ -57,6 +31,93 @@ defmodule WebGames.Minesweeper.GameStateTest do
       game_type: :custom,
       end_game_ref: nil,
     }}}
+  end
+
+  defp empty_grid() do
+    %{
+      {1,1} => %Cell{},
+      {1,2} => %Cell{},
+      {1,3} => %Cell{},
+      {1,4} => %Cell{},
+
+      {2,1} => %Cell{},
+      {2,2} => %Cell{},
+      {2,3} => %Cell{},
+      {2,4} => %Cell{},
+
+      {3,1} => %Cell{},
+      {3,2} => %Cell{},
+      {3,3} => %Cell{},
+      {3,4} => %Cell{},
+
+      {4,1} => %Cell{},
+      {4,2} => %Cell{},
+      {4,3} => %Cell{},
+      {4,4} => %Cell{},
+
+      {5,1} => %Cell{},
+      {5,2} => %Cell{},
+      {5,3} => %Cell{},
+      {5,4} => %Cell{},
+    }
+  end
+
+  defp mined_grid() do
+    %{
+      {1,1} => %Cell{value: 1},
+      {1,2} => %Cell{value: 1, has_mine?: true},
+      {1,3} => %Cell{value: 1},
+      {1,4} => %Cell{},
+
+      {2,1} => %Cell{value: 1},
+      {2,2} => %Cell{value: 1},
+      {2,3} => %Cell{value: 2},
+      {2,4} => %Cell{value: 1},
+
+      {3,1} => %Cell{},
+      {3,2} => %Cell{},
+      {3,3} => %Cell{value: 1},
+      {3,4} => %Cell{value: 1, has_mine?: true},
+
+      {4,1} => %Cell{},
+      {4,2} => %Cell{},
+      {4,3} => %Cell{value: 1},
+      {4,4} => %Cell{value: 1},
+
+      {5,1} => %Cell{},
+      {5,2} => %Cell{},
+      {5,3} => %Cell{},
+      {5,4} => %Cell{},
+    }
+  end
+
+  defp almost_opened_grid() do
+    %{
+      {1,1} => %Cell{value: 1, opened?: true},
+      {1,2} => %Cell{value: 1, has_mine?: true},
+      {1,3} => %Cell{value: 1, opened?: true},
+      {1,4} => %Cell{opened?: true},
+
+      {2,1} => %Cell{value: 1, opened?: true},
+      {2,2} => %Cell{value: 1, opened?: true},
+      {2,3} => %Cell{value: 2, opened?: true},
+      {2,4} => %Cell{value: 1}, # <-- Last unopened cell
+
+      {3,1} => %Cell{opened?: true},
+      {3,2} => %Cell{opened?: true},
+      {3,3} => %Cell{value: 1, opened?: true},
+      {3,4} => %Cell{value: 1, has_mine?: true},
+
+      {4,1} => %Cell{opened?: true},
+      {4,2} => %Cell{opened?: true},
+      {4,3} => %Cell{value: 1, opened?: true},
+      {4,4} => %Cell{value: 1, opened?: true},
+
+      {5,1} => %Cell{opened?: true},
+      {5,2} => %Cell{opened?: true},
+      {5,3} => %Cell{opened?: true},
+      {5,4} => %Cell{opened?: true},
+    }
   end
 
   test "init sets state based on config" do
@@ -303,7 +364,7 @@ defmodule WebGames.Minesweeper.GameStateTest do
     |> Map.put(:player, "playerid_1")
     |> Map.put(:status, :play)
 
-    {:ok, msgs, new_state} = GameState.handle_event(state, "playerid_1", {:flag, {0,0}})
+    {:ok, msgs, new_state} = GameState.handle_event(state, "playerid_1", {:flag, {-2,0}})
 
     assert msgs === []
     assert new_state.grid === state.grid
@@ -335,16 +396,235 @@ defmodule WebGames.Minesweeper.GameStateTest do
     assert new_state.grid === state.grid
   end
 
-  """
-  TODO:
-  mines are placed on :open when state is :init
-  cell is opened on :open when state is :play
-  adjacent cells are opened on :open when cell has 0 value
-  send failed message when opened cell has mine
-  send win message when opened last unmined cell
-  cell is not opened when state is :win or :lose
-  cell is not opened when cell has a flag
-  nothing happens when opening cell outside of grid
-  """
+  # TODO: This sometimes fails due to randomness
+  # If the mines are placed in a certain way, it's possible to win in one move.
+  # Meaning the output is going to be different.
+  # Need to find a way to fix the mine placement results.
+  @tag :skip
+  test ":open opens cell and places initial mines when status is :init", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :init)
+    |> Map.put(:grid, empty_grid())
 
+    with_mock DateTime, [utc_now: fn() -> ~U[2024-01-06 23:25:38.371659Z] end] do
+      {:ok, msgs, new_state} = GameState.handle_event(state, "playerid_1", {:open, {1,1}})
+
+      assert new_state.status === :play
+      assert new_state.grid[{1,1}].opened?
+      assert new_state.grid[{1,1}].value === 0
+
+      # Adjacent cells are also open, since initial value MUST be 0
+      assert new_state.grid[{1,2}].opened?
+      assert new_state.grid[{2,1}].opened?
+      assert new_state.grid[{2,2}].opened?
+
+      assert Enum.count(new_state.grid, fn {_, cell} -> cell.has_mine? end) === 2
+      [
+        %PubSubMessage{to: :all, type: :game_event, payload: [
+          {:open, open_cells},
+          {:click, clicked_cells},
+          {:start_game, ~U[2024-01-06 23:25:38.371659Z]},
+        ]},
+      ] = msgs
+
+      # We can't know the exact list here - it's random based on where
+      # the mines ended up.
+      assert open_cells |> is_map()
+      assert clicked_cells === %{{1,1} => true}
+    end
+  end
+
+  test ":open opens cell with non-zero value on :play", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :play)
+
+    {:ok, msgs, new_state} = GameState.handle_event(state, "playerid_1", {:open, {1,1}})
+
+    assert new_state.status === :play
+    assert new_state.grid[{1,1}].opened?
+    assert Enum.count(new_state.grid, fn {_, cell} -> cell.opened? end) === 1
+
+    assert msgs === [
+      %PubSubMessage{to: :all, type: :game_event, payload: [{:open, %{{1,1} => 1}}, {:click, %{{1,1} => true}}]},
+    ]
+  end
+
+  test ":open opens all adjacent cells when cell value is 0", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :play)
+
+    {:ok, msgs, new_state} = GameState.handle_event(state, "playerid_1", {:open, {5,1}})
+
+    #  |12345  x
+    # -------
+    # 1|11...<-- Cascade off this corner
+    # 2|X1...
+    # 3|1211.
+    # 4|.1X1.
+    #
+    # y
+
+    assert new_state.status === :play
+    assert new_state.grid[{5,1}].opened?
+    assert new_state.grid[{5,1}].value === 0
+    assert new_state.grid[{5,1}].clicked?
+
+    assert new_state.grid[{2,1}].opened?
+    assert new_state.grid[{2,1}].value === 1
+    assert new_state.grid[{3,1}].opened?
+    assert new_state.grid[{3,1}].value === 0
+    assert new_state.grid[{4,1}].opened?
+    assert new_state.grid[{4,1}].value === 0
+
+    assert new_state.grid[{2,2}].opened?
+    assert new_state.grid[{2,2}].value === 1
+    assert new_state.grid[{3,2}].opened?
+    assert new_state.grid[{3,2}].value === 0
+    assert new_state.grid[{4,2}].opened?
+    assert new_state.grid[{4,2}].value === 0
+    assert new_state.grid[{5,2}].opened?
+    assert new_state.grid[{5,2}].value === 0
+
+    assert new_state.grid[{2,3}].opened?
+    assert new_state.grid[{2,3}].value === 2
+    assert new_state.grid[{3,3}].opened?
+    assert new_state.grid[{3,3}].value === 1
+    assert new_state.grid[{4,3}].opened?
+    assert new_state.grid[{4,3}].value === 1
+    assert new_state.grid[{5,3}].opened?
+    assert new_state.grid[{5,3}].value === 0
+
+    assert new_state.grid[{4,4}].opened?
+    assert new_state.grid[{4,4}].value === 1
+    assert new_state.grid[{5,4}].opened?
+    assert new_state.grid[{5,4}].value === 0
+
+    assert msgs === [
+      %PubSubMessage{to: :all, type: :game_event, payload: [
+        {:open, %{
+          {5,1} => 0,
+
+          {2,1} => 1,
+          {3,1} => 0,
+          {4,1} => 0,
+
+          {2,2} => 1,
+          {3,2} => 0,
+          {4,2} => 0,
+          {5,2} => 0,
+
+          {2,3} => 2,
+          {3,3} => 1,
+          {4,3} => 1,
+          {5,3} => 0,
+
+          {4,4} => 1,
+          {5,4} => 0,
+          }},
+        {:click, %{{5,1} => true}}
+      ]},
+    ]
+  end
+
+  test ":open ends the game when opening a mine", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :play)
+
+    with_mocks([
+      {DateTime, [], [utc_now: fn() -> ~U[2024-01-06 23:25:38.371659Z] end]},
+      {InternalComms, [], [schedule_end_game: fn(_) -> Kernel.make_ref() end]}
+     ]) do
+      {:ok, msgs, new_state} = GameState.handle_event(state, "playerid_1", {:open, {1,2}})
+
+      assert new_state.grid[{1,2}].clicked?
+      assert new_state.grid[{1,2}].opened?
+      assert new_state.status === :lose
+      assert new_state.end_time === ~U[2024-01-06 23:25:38.371659Z]
+      assert new_state.end_game_ref |> is_reference()
+
+      assert msgs === [
+        %PubSubMessage{to: :all, type: :game_event, payload: [
+          {:open, %{{1, 2} => 1}},
+          {:show_mines, [{1, 2}, {3, 4}]},
+          {:game_over, %{status: :lose, end_time: ~U[2024-01-06 23:25:38.371659Z]}},
+          {:click, %{{1, 2} => true}}
+        ]}
+      ]
+    end
+  end
+
+  test ":open ends the game when opening the last unopened mine", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :play)
+    |> Map.put(:grid, almost_opened_grid())
+    # The last unopened cell is {2,4}
+
+    with_mocks([
+      {DateTime, [], [utc_now: fn() -> ~U[2024-01-06 23:25:38.371659Z] end]},
+      {InternalComms, [], [schedule_end_game: fn(_) -> Kernel.make_ref() end]}
+     ]) do
+      {:ok, msgs, new_state} = GameState.handle_event(state, "playerid_1", {:open, {2,4}})
+
+      assert new_state.grid[{2,4}].clicked?
+      assert new_state.grid[{2,4}].opened?
+      assert new_state.status === :win
+      assert new_state.end_time === ~U[2024-01-06 23:25:38.371659Z]
+      assert new_state.end_game_ref |> is_reference()
+
+      assert msgs === [
+        %PubSubMessage{to: :all, type: :game_event, payload: [
+          {:show_mines, [{1, 2}, {3, 4}]},
+          {:game_over, %{status: :win, end_time: ~U[2024-01-06 23:25:38.371659Z]}},
+          {:open, %{{2, 4} => 1}},
+          {:click, %{{2, 4} => true}}
+        ]}
+      ]
+    end
+  end
+
+  test ":open ignores command when in :win status", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :win)
+
+    {:ok, [], new_state} = GameState.handle_event(state, "playerid_1", {:open, {1,1}})
+
+    assert new_state === state
+  end
+
+  test ":open ignores command when in :lose status", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :lose)
+
+    {:ok, [], new_state} = GameState.handle_event(state, "playerid_1", {:open, {1,1}})
+
+    assert new_state === state
+  end
+
+  test ":open does nothing when opening a flagged cell", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :play)
+    |> Map.put(:grid, %{state.grid | {1,1} => %{state.grid[{1,1}] | flagged?: true}})
+
+    {:ok, [], new_state} = GameState.handle_event(state, "playerid_1", {:open, {1,1}})
+
+    assert new_state === state
+  end
+
+  test ":open does nothing when opening a cell that is off-grid", %{state: state} do
+    state = state
+    |> Map.put(:player, "playerid_1")
+    |> Map.put(:status, :play)
+
+    {:ok, [], new_state} = GameState.handle_event(state, "playerid_1", {:open, {-2,0}})
+
+    assert new_state === state
+  end
 end
